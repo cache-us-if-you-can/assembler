3943a438a05aab0a0964c3c03da84354da797c4b
anders130 <93037023+anders130@users.noreply.github.com>
2025-05-15 11:04:30 +0200

    wip parse input via nix

--- parser.nix
+++ parser.nix
@@ -3,14 +3,37 @@
     input,
 }: let
     inherit (pkgs) lib;
-    inherit (lib.strings) splitString;
-    inherit
-        (builtins)
-        readFile
-        filter
-        ;
+    inherit (lib.strings) splitString toUpper;
+    inherit (lib.lists) imap0;
+    inherit (builtins) readFile filter match elem;
+
+    instructions = [
+        "nop"
+        "input"
+        "output"
+        "jmp"
+        "load"
+        "inc"
+        "mov"
+        "add"
+        "halt"
+    ];
+
+    strContains = str: substr: elem substr (splitString "" str);
 in
     input
     |> readFile
     |> splitString "\n"
     |> filter (line: line != "")
+    # handle each line
+    |> imap0 (
+        i: line:
+            line
+            |> splitString " "
+            |> filter (word: word != "")
+            |> map toUpper
+            |> map (word:
+                if strContains word ":"
+                then word
+                else "${word}:")
+    )
